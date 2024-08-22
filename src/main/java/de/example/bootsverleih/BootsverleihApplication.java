9e607b0af5c42a7feea49f941558d75476bcde24
andreashl09 <a9279352@gmail.com>
2024-08-22 22:19:17 +0200

    kickoff

---
+++ src/main/java/de/example/bootsverleih/BootsverleihApplication.java
@@ -0,0 +1,13 @@
+package de.example.bootsverleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootsverleihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootsverleihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/example/bootsverleih/BootsverleihApplicationTests.java
@@ -0,0 +1,13 @@
+package de.example.bootsverleih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootsverleihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
